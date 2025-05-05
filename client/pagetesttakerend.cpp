2fadd862c6c53e791ebcf31203b1336a7e67a916
0x110 <Sashik95@yandex.ru>
2025-05-05 09:17:04 +0500

    Добавил текст показа результата тестирования

--- client/pagetesttakerend.cpp
+++ client/pagetesttakerend.cpp
@@ -5,12 +5,18 @@
 #include "enumpages.h"
 
 PageTestTakerEnd::PageTestTakerEnd(MainWindow* parent) : QWidget{parent} {
-    labelResult = new QLabel("0", this);
+    labelResult = new QLabel("null", this);
     QVBoxLayout* layout = new QVBoxLayout(this);
+    QHBoxLayout* layoutResults = new QHBoxLayout(this);
+    layoutResults->setAlignment(Qt::AlignLeft);
     QPushButton* buttonBack = new QPushButton("В главное меню", this);
+    QLabel* labelTextResult = new QLabel("Ваш результат:", this);
+
+    layoutResults->addWidget(labelTextResult);
+    layoutResults->addWidget(labelResult);
 
     layout->addWidget(buttonBack);
-    layout->addWidget(labelResult);
+    layout->addLayout(layoutResults);
 
     connect(buttonBack, &QPushButton::clicked, this, [parent]() {
         parent->SetPage((int)Pages::PageMain);
